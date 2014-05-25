17f00ea83d41bb9fdc36c70f05c95dcbaf535c2d
Thempra <thempra@overxet.com>
2014-05-25 11:47:53 +0200

    Fixed monitor script  bugs

--- src/overmind_monitor.sh
+++ src/overmind_monitor.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 Cmd="./overmind_logger"
 Log="./samples/overmind.csv"
-#Dev="/dev/rfcomm0"
-Dev=$1
+Dev="/dev/rfcomm0"
+
 
 echo "x,y1,y2,y3,y4,y5,y6,y7,y8,y9,y10,y11"  > $Log
 echo "seconds, signals, attention, meditation, delta, theta, low alpha, high alpha, low beta, high beta, low gamma, high gamma"  >>$Log
